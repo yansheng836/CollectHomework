dea896d75b418b9a1c04f8e3544275e498184eab
yansheng836 <1812871846@qq.com>
2019-06-22 22:50:28 +0800

    为FindStudentUtil及其测试类添加了部分注释

--- CollectHomework/src/main/java/com/ys/util/FindStudentUtil.java
+++ CollectHomework/src/main/java/com/ys/util/FindStudentUtil.java
@@ -15,7 +15,7 @@ import com.ys.bean.Student;
 
 /**
  * <p>Title: </p>
- * <p>Description: </p>
+ * <p>Description: 定义和查找学生相关的方法。</p>
  * <p>Company: </p>
  * @author yansheng
  * @date 2019-06-21 00:48:30
@@ -26,34 +26,22 @@ public class FindStudentUtil {
 	/**
 	 * @Title findBySname
 	 * @author yansheng
-	 * @version v1.0
-	 * @date 2019-06-22 00:36:35
-	 * @Description 通过（文件名与点名册中的）姓名对比，查找已交作业的学生集合。
-	 * @param students	点名册中的学生集合
-	 * @param pathList	文件路径名集合
-	 * @return   
-	 * ArrayList<Student> 找到的学生列表
-	 * @see  Student /CollectHomework/src/test/java/com/ys/main/TestList1.java
-	 */
-	// 为了减少代码的重复率，直接将按姓名进行检索和按学号进行检索合并在一个方法里面。
-	/**
-	 * @Title findBySname
-	 * @author yansheng
-	 * @version v1.0
+	 * @version v2.0
 	 * @date 2019-06-22 17:30:15
-	 * @Description TODO
-	 * @param students
-	 * @param fileList
+	 * @Description 通过（文件名与点名册中的）姓名对比，查找已交作业的学生集合；为了减少代码的重复率，直接将按姓名进行检索和按学号进行检索合并在一个方法里面。。
+	 * @param students 点名册中的学生集合
+	 * @param fileList 文件路径名集合
 	 * @param sign
 	 * @return   
 	 * ArrayList<ArrayList<Student>> 一个包含ArrayList<Student>对象的列表，第一个值为已找到的学生列表，第二个值为未找到的学生列表
-	 * @see  
-	 * @exception
+	 * @see  Student /CollectHomework/src/test/java/com/ys/main/TestList1.java
 	 */
-	public static ArrayList<ArrayList<Student>> findBySname(ArrayList<Student> students,
+	public static ArrayList<ArrayList<Student>> findStu(ArrayList<Student> students,
 			ArrayList<String> fileList, String sign) {
-		// 1.先判断是“按姓名进行检索和按学号进行检索”？
-		// 定义一个布尔变量，如果是true表示为sno,false为sname
+
+		/*
+		 * 1.先判断是“按姓名进行检索”还是“按学号进行检索”？ 定义一个布尔变量bool，如果是true表示为sno,false为sname。
+		 */
 		String snoSign = "sno";
 		String snameSign = "sname";
 		boolean bool = false;
@@ -63,14 +51,13 @@ public class FindStudentUtil {
 		} else if (snameSign.equalsIgnoreCase(sign)) {
 			bool = false;
 		}
-		System.out.println("bool的值为：" + bool);
+		// System.out.println("bool的值为：" + bool);
 
-		// 2.
-
-		// 2.定义一个学生列表，用于存放匹配到的学生
+		// 2. 定义一个存放学生列表的列表，第一个值为已找到的学生列表，第二个值为未找到的学生列表。
 		ArrayList<ArrayList<Student>> arrayStuList = new ArrayList<ArrayList<Student>>();
 
 		ArrayList<Student> foundStuList = new ArrayList<Student>();
+		// 为了方便理解，定义一个学生列表，存放未找到的学生（其实如果找到就移除的话，剩下的学生列表就是未找到的学生列表）
 		ArrayList<Student> noFoundStuList = new ArrayList<Student>();
 
 		// 改进，在遍历时如果找到,移除该元素,减少循环。为防止出现异常，用Iterator遍历，而不用foreach遍历。
@@ -81,11 +68,15 @@ public class FindStudentUtil {
 		Iterator<String> iterPath = fileList.iterator();
 		String path;
 		Student student;
+		// 存放学号或者是姓名，根据bool变量的值，赋予不同值
+		// 如果bool是true，则说明是按照学号进行检索，那么judgeSign为每个学生的学号，否则为每个学生的姓名。
 		String judgeSign;
+
+		// 路径名列表 匹配 学生列表，如果一个路径名匹配到一个学生，将该学生加到foundStuList，并立即删除原学生列表中的该学生，减少下次循环次数，跳出循环
+		//
 		while (iterPath.hasNext()) {
 			path = iterPath.next();
 			// System.out.println("\n----path:" + path);
-
 			while (iterStu.hasNext()) {
 				student = iterStu.next();
 				// System.out.println("----student:" + student);
@@ -103,7 +94,7 @@ public class FindStudentUtil {
 					foundStuList.add(student);
 
 					// 找到后从文件列表和学生列表中移除该项，跳出改成循环
-					iterPath.remove();
+					// iterPath.remove(); //路径列表可以不进行移除，因为后面不需要再重复遍历该条记录
 					iterStu.remove();
 
 					break;
@@ -115,40 +106,54 @@ public class FindStudentUtil {
 		}
 
 		// 将已找到学生列表和未找到学生列表添加到arrayStuList列表中。
+		noFoundStuList = students;
 		arrayStuList.add(foundStuList);
-		arrayStuList.add(students);
+		arrayStuList.add(noFoundStuList);
 
-		System.out.println("\n最后进行遍历：");
-		for (Student student1 : students) {
-			System.out.println(student1);
-		}
-		for (String student1 : fileList) {
-			System.out.println(student1);
-		}
-
-		System.out.println("遍历：ArrayList<ArrayList<Student>> arrayList");
-		for (ArrayList<Student> object : arrayStuList) {
-			System.out.println(object);
-		}
+		// System.out.println("\n最后进行遍历：");
+		// for (Student student1 : students) {
+		// System.out.println("students:"+student1);
+		// }
+		// for (String student1 : fileList) {
+		// System.out.println("fileList:"+student1);
+		// }
+		//
+		// System.out.println("遍历：ArrayList<ArrayList<Student>> arrayList");
+		// for (ArrayList<Student> list : arrayStuList) {
+		// System.out.println(list);
+		// }
 
 		return arrayStuList;
 	}
 
-	public static String compareNumber(ArrayList<Student> students, ArrayList<String> fileList) {
-		
-		int stuLen = students.size();
-		int fileLen = fileList.size();
-		System.out.println("stuLen:"+stuLen);
-		System.out.println("fileLen:"+fileLen);
-		
-		String string = null ;
-		if (stuLen == fileLen) {
-			string = "文件数量和学生列表数量一致！\n"+"学生列表有"+stuLen+"个学生，文件列表有"+fileLen+"个文件。";
+	/**
+	 * @Title compareNumber
+	 * @author yansheng
+	 * @version v1.0
+	 * @date 2019-06-22 22:28:53
+	 * @Description 比较班级学生人数和文件夹内的文件数目是否一致
+	 * @param students
+	 * @param fileList
+	 * @return   
+	 * String[] 字符串数组（字符串，学生数量，文件数量）
+	 */
+	public static String[] compareNumber(ArrayList<Student> students, ArrayList<String> fileList) {
+
+		Integer stuNum = students.size();
+		Integer fileNum = fileList.size();
+		// System.out.println("stuLen:"+stuLen);
+		// System.out.println("fileLen:"+fileLen);
+
+		String string = null;
+		if (stuNum == fileNum) {
+			string = "文件数量和学生列表数量一致！" + "学生列表有" + stuNum + "个学生，文件列表有" + fileNum + "个文件。";
 		} else {
-			string = "文件数量和学生列表数量不一致！\n"+"学生列表有"+stuLen+"个学生，文件列表有"+fileLen+"个文件。";
+			string = "文件数量和学生列表数量不一致！" + "学生列表有" + stuNum + "个学生，文件列表有" + fileNum + "个文件。";
 		}
 
-		return string;
+		String[] compareResults = { string, stuNum.toString(), fileNum.toString() };
+
+		return compareResults;
 
 	}
 
--- CollectHomework/src/test/java/com/ys/util/TestFindStudentUtil.java
+++ CollectHomework/src/test/java/com/ys/util/TestFindStudentUtil.java
@@ -15,7 +15,7 @@ import com.ys.bean.Student;
 
 /**
  * <p>Title: </p>
- * <p>Description: </p>
+ * <p>Description: 测试FindStudentUtil的方法</p>
  * <p>Company: </p>
  * @author yansheng
  * @date 2019-06-21 00:49:49
@@ -31,9 +31,8 @@ public class TestFindStudentUtil {
 	 * @date 2019-06-21 00:49:49
 	 * @Description TODO
 	 * @param args   
-	 * void 
+	 * @return void 
 	 * @see  
-	 * @exception
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
 		String excelPath = "16计算机科学与技术3学生名单.xls";
@@ -47,20 +46,28 @@ public class TestFindStudentUtil {
 		String dirPath = "E:\\1学习，作业，文档\\6大三下相关文档资料及作业\\收作业\\16计科3班Linux实验报告";
 		ArrayList<String> fileList = FileUtil.readDir(dirPath);
 		
-		String string = FindStudentUtil.compareNumber(students, fileList);
-		System.out.println("string:" + string);
+		// 测试“比较比较班级学生人数和文件夹内的文件数目”的方法
+		String[] compareResults = FindStudentUtil.compareNumber(students, fileList);
+		String result = compareResults[0];
+		int stuNum = Integer.parseInt(compareResults[1]);
+		int fileNum = Integer.parseInt(compareResults[2]);
+				
+		System.out.println("result:"+result);
+		System.out.println("stuNum:"+stuNum);
+		System.out.println("fileNum:"+fileNum);
 		
-
+		
+		// 测试“比较比较班级学生人数和文件夹内的文件数目”的方法
 		String sign = "sno";
-		ArrayList<ArrayList<Student>> arrayStuList = FindStudentUtil.findBySname(students, fileList, sign);
+		ArrayList<ArrayList<Student>> arrayStuList = FindStudentUtil.findStu(students, fileList, sign);
 		ArrayList<Student> studentFindList = arrayStuList.get(0);
 		ArrayList<Student> noFoundStuList = arrayStuList.get(1);
-		System.out.println("在TestFindStudentUtil中遍历：");
+		System.out.println("\n在TestFindStudentUtil中遍历已交作业的学生：");
 		for (Student student : studentFindList) {
 			System.out.println(student);
 		}
 
-		System.out.println("不存在的student中遍历：");
+		System.out.println("\n遍历未交作业的学生：");
 		for (Student student : noFoundStuList) {
 			System.out.println(student);
 		}
